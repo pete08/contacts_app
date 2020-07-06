e82fc80d19cba7f252091b45653c10321b9f0723
PeterH <pete08@gmail.com>
2020-07-06 18:44:05 -0500

    Add Create request RESTfull API

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -10,7 +10,16 @@ def show
   @contact = Contact.find_by(id: params[:id])
   render "show.json.jb"
 end
-# def create
+
+def create
+  @contact = Contact.new(first_name: params[:first], last_name: params[:last], email: params[:email], phone_number: params[:number])
+  @contact.save
+  render "show.json.jb"
+end
+
 # def update
 # def destroy
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
 # def index #COMPLETE
 # def show #COMPLETE
-# def create
+# def create #COMPLETE
 # def update
 # def destroy
 
